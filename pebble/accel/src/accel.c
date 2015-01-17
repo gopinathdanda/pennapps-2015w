2e92c83d922adb0eadeb3a2c239fb2369167b89b
Gopinath Danda <gopinath.danda@gmail.com>
2015-01-16 23:15:44 -0500

    Sleep and wake up bot controls

--- pebble/accel/src/accel.c
+++ pebble/accel/src/accel.c
@@ -8,13 +8,21 @@ static TextLayer *accel_layer;
 
 static AppTimer *timer;
 
+static bool motor;
+
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(accel_layer, "Move up");
 }
 
-/*static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(accel_layer, "Select pressed!");
-}*/
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if(!motor){
+	motor = true;
+  	text_layer_set_text(accel_layer, "Waking up");
+  }else{
+	motor = false;
+	text_layer_set_text(accel_layer, "Sleeping");
+  }
+}
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(accel_layer, "Move down");
@@ -23,7 +31,7 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 static void click_config_provider(void *context) {
   // Register the ClickHandlers
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  //window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
@@ -32,29 +40,34 @@ static void timer_callback(void *data) {
 
   // Get AccelData in accel variable
   accel_service_peek(&accel);
-  static char l[0x100];
+  static char move[0x100];
 
-  // Logic
-  if((accel.x<500 && accel.x>-500) && accel.y>500){
-	snprintf(l, sizeof(l), "Turn right");
-  }else if((accel.x<500 && accel.x>-500) && accel.y<-500){
-	snprintf(l, sizeof(l), "Turn left");
-  }else if(accel.x>500 && (accel.y<500 && accel.y>-500)){
-	snprintf(l, sizeof(l), "Go back");
-  }else if(accel.x<-500 && (accel.y<500 && accel.y>-500)){
-	snprintf(l, sizeof(l), "Go ahead");
-  }else if(accel.x>500 && accel.y<-500){
-	snprintf(l, sizeof(l), "Go back and left");
-  }else if(accel.x<-500 && accel.y<-500){
-	snprintf(l, sizeof(l), "Go ahead and left");
-  }else if(accel.x>500 && accel.y>500){
-	snprintf(l, sizeof(l), "Go back and right");
-  }else if(accel.x<-500 && accel.y>500){
-	snprintf(l, sizeof(l), "Go ahead and right");
+  if(motor){
+  	// Logic
+	  if((accel.x<500 && accel.x>-500) && accel.y>500){
+		snprintf(move, sizeof(move), "Turn right");
+	  }else if((accel.x<500 && accel.x>-500) && accel.y<-500){
+		snprintf(move, sizeof(move), "Turn left");
+	  }else if(accel.x>500 && (accel.y<500 && accel.y>-500)){
+		snprintf(move, sizeof(move), "Go back");
+	  }else if(accel.x<-500 && (accel.y<500 && accel.y>-500)){
+		snprintf(move, sizeof(move), "Go ahead");
+	  }else if(accel.x>500 && accel.y<-500){
+		snprintf(move, sizeof(move), "Go back and left");
+	  }else if(accel.x<-500 && accel.y<-500){
+		snprintf(move, sizeof(move), "Go ahead and left");
+	  }else if(accel.x>500 && accel.y>500){
+		snprintf(move, sizeof(move), "Go back and right");
+	  }else if(accel.x<-500 && accel.y>500){
+		snprintf(move, sizeof(move), "Go ahead and right");
+	  }else{
+		snprintf(move, sizeof(move), "Stay");
+  	  }
   }else{
-	snprintf(l, sizeof(l), "Stay");
+	  snprintf(move, sizeof(move), "Sleeping");
   }
-  text_layer_set_text(accel_layer, l);
+  
+  text_layer_set_text(accel_layer, move);
 
   timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
 }
@@ -62,14 +75,16 @@ static void timer_callback(void *data) {
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
+  motor = false;
   accel_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
   text_layer_set_font(accel_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(accel_layer, "No data yet.");
+  text_layer_set_text(accel_layer, "Sleeping");
   text_layer_set_overflow_mode(accel_layer, GTextOverflowModeWordWrap);
   text_layer_set_background_color(accel_layer, GColorWhite);
   text_layer_set_text_color(accel_layer, GColorBlack);
   text_layer_set_text_alignment(accel_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(accel_layer));
+
 }
 
 static void window_unload(Window *window) {
